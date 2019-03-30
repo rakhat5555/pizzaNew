ec045e303a25f219fd5240e48d2880e25fcf562d
Student <example@example.com>
2019-03-29 18:02:12 -0700

    add input for addresses

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,16 @@ Menu.prototype.getPrice = function() {
 return(sum)
   console.log(sum);
 }
-
-
+function Address(street, city, state, zip, apt) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
+  this.apt = apt;
+}
+Address.prototype.fullAddress =  function() {
+  return this.street + " " + this.city + ", " + this.state + " " + this.zip + " " + this.apt;
+}
 
 // interface logic
 $(document).ready(function() {
@@ -67,6 +75,25 @@ var menuList = new Menu()
           $("#result2").show();
           $("#order2").hide();
 
-
         });
-      });
+// deliver section
+    $(".deliver").click(function() {
+      $("#address").show();
+    });
+    // var addressInput = new Address;
+    $("form#address").submit(function(event) {
+      event.preventDefault();
+      var addresses = ['street', 'city', 'state', 'zip', 'apt'];
+       addresses.forEach(function(address) {
+        var addressInput = $("input#" + address).val();
+         $("." + address).text(addressInput)
+       });
+       $("#addressShow").show();
+       $("#address").hide();
+       $(".deliver").hide();
+    })
+
+
+
+
+});
